eaa1d143a6ca94a0a320cc984749e00a77a76b8c
Chebbi Ahmed <98619224+Ahmed5827@users.noreply.github.com>
2024-08-26 16:36:41 +0100

    Delete src directory

--- src/csvapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CSVFile
-
-class CSVFileForm(forms.ModelForm):
-    class Meta:
-        model = CSVFile  # This form is tied to the CSVFile model
-        fields = ['file']  # This form will have a single field for the file upload
